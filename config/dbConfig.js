7d11d99aaf042abcb164e572c86dba2c6576b5c3
Uygar <komifb90@hotmail.com>
2024-11-16 12:53:08 +0300

    .env

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
+
 const sql = require('mssql');
 
 const db = {
-    user: 'sa',
-    password: 'uygar123',
-    server: 'UYGAR\\SQLEXPRESS', 
-    database: 'SiteManagementDB',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    server: process.env.DB_SERVER,
+    database: process.env.DB_DATABASE,
     options: {
-        encrypt: true,
-        trustServerCertificate: true
+        encrypt: process.env.DB_ENCRYPT === 'true',
+        trustServerCertificate: process.env.DB_TRUST_SERVER_CERTIFICATE === 'true'
     }
 };
 
-module.exports = db; 
+module.exports = db;
